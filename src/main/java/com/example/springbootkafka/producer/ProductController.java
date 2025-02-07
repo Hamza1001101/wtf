bdabcb78c66c7199bc8cc8d954630618fab0736c
Kian Farah <dado.org@hotmail.com>
2025-02-07 08:50:08 +0100

    deleted creds

--- src/main/java/com/example/springbootkafka/producer/ProductController.java
+++ src/main/java/com/example/springbootkafka/producer/ProductController.java
@@ -1,29 +1,29 @@
-//package com.example.springbootkafka.producer;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/products")
-//public class ProductController {
-//
-//    final ProductService service;
-//
-//    public ProductController(ProductService service) {
-//        this.service = service;
-//    }
-//
-//
-//    @ResponseStatus(HttpStatus.OK)
-//    @GetMapping
-//    public List<Product> findAll() {
-//        return service.findAll();
-//    }
-//
-//    @PostMapping
-//    public void save(@RequestBody Product product) {
-//        service.save(product);
-//    }
-//}
+package com.example.springbootkafka.producer;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<Product> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody Product product) {
+        service.save(product);
+    }
+}
